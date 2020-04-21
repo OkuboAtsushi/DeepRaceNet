1d36010366e60f5a5d1130b1933eaaa17dbd0415
OkuboAtsushi <a2c009bo@gmail.com>
2020-04-21 18:49:16 +0900

    add setup script

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Horse race prediction using deep learning',
+    author='OkuboAtsushi',
+    license='MIT',
+)
